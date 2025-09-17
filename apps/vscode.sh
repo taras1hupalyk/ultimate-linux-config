aa893eb37a0b54562bfcf5bc14a061696b4e6151
Taras Hupalyk <t.hupalyk@gmail.com>
2025-09-17 16:41:35 +0300

    Add temporary vscode download link

--- apps/vscode.sh
+++ apps/vscode.sh
@@ -1,6 +1,13 @@
 
 
-wget   https://code.visualstudio.com/docs/?dv=linux64_rpm -O /tmp/vscode.rpm
+wget   https://vscode.download.prss.microsoft.com/dbazure/download/stable/f220831ea2d946c0dcb0f3eaa480eb435a2c1260/code-1.104.0-1757488045.el8.x86_64.rpm
+ -O /tmp/vscode.rpm
+
 
 sudo dnf install /tmp/vscode.rpm -y
 
+
+# ??? how long will this link be alive??
+#https://vscode.download.prss.microsoft.com/dbazure/download/stable/f220831ea2d946c0dcb0f3eaa480eb435a2c1260/code-1.104.0-1757488045.el8.x86_64.rpm
+
+#https://vscode.download.prss.microsoft.com/dbazure/download/stable/f220831ea2d946c0dcb0f3eaa480eb435a2c1260/code-1.104.0-1757488045.el8.x86_64.rpm
