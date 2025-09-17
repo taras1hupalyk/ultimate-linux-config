031e6cc2805406fbb47a01b485f99b27ff818850
Taras Hupalyk <t.hupalyk@gmail.com>
2025-09-17 16:45:44 +0300

    Run AppImageLauncher as a regular user not with sudo

--- apps/obsidian.sh
+++ apps/obsidian.sh
@@ -1,5 +1,5 @@
 wget  https://github.com/obsidianmd/obsidian-releases/releases/download/v1.9.10/Obsidian-1.9.10.AppImage  -O /tmp/Obsidian-1.9.10.AppImage
 
 
-sudo AppImageLauncher /tmp/Obsidian-1.9.10.AppImage
+AppImageLauncher /tmp/Obsidian-1.9.10.AppImage
 
